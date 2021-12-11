2b409e65fb7235db1292ac92cc2bed0a63464d01
kla04 <fiw56789@gmail.com>
2021-12-11 13:48:06 +0700

    get post delete for admin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ var subjects = [
     {
         "subjectID": "240-101",
         "subjectName": "Intro to Computer Programming",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
         "registered": 39,
@@ -22,223 +22,223 @@ var subjects = [
     {
         "subjectID": "240-203",
         "subjectName": "Computer Engineering Software Laboratory II",
-        "weight": 1,
+        "credit": 1,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 38,
+        "registered": 0,
         "maximum": 40
     },
     {
         "subjectID": "240-204",
         "subjectName": "Computer Engineering Hardware Laboratory II",
-        "weight": 1,
+        "credit": 1,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 19,
+        "registered": 0,
         "maximum": 50
     },
     {
         "subjectID": "240-205",
         "subjectName": "Electric Circuits",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 19,
+        "registered": 0,
         "maximum": 30
     },
     {
         "subjectID": "240-206",
         "subjectName": "Intro to Computer Networks",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 20,
+        "registered": 0,
         "maximum": 30
     },
     {
         "subjectID": "240-207",
         "subjectName": "Programming and Data Structures",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 39,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-208",
         "subjectName": "Digital Logic and Design",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 49,
+        "registered": 0,
         "maximum": 50
     },
     {
         "subjectID": "240-209",
         "subjectName": "Basic Electronics",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 48,
+        "registered": 0,
         "maximum": 70
     },
     {
         "subjectID": "240-210",
         "subjectName": "Programming Technique",
-        "weight": 2,
+        "credit": 2,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-211",
         "subjectName": "Software Engineering",
-        "weight": 2,
+        "credit": 2,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 41,
+        "registered": 0,
         "maximum": 50
     },
     {
         "subjectID": "240-212",
         "subjectName": "Probability and Statistic",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 57,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-213",
         "subjectName": "Discrete Mathematics",
-        "weight": 2,
+        "credit": 2,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-214",
         "subjectName": "Data Communication and Networking",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 52,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-215",
         "subjectName": "Computer Engineering Mathematics",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 80
     },
     {
         "subjectID": "240-301",
         "subjectName": "Advanced Computer Engineering Laboratory I",
-        "weight": 1,
+        "credit": 1,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 59,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-302",
         "subjectName": "Advanced Computer Engineering Laboratory II",
-        "weight": 1,
+        "credit": 1,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 70
     },
     {
         "subjectID": "240-303",
         "subjectName": "Ethical, Legal and Social Issues",
-        "weight": 1,
+        "credit": 1,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-304",
         "subjectName": "Computer Operating System",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 39,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-305",
         "subjectName": "Database Systems",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 41,
+        "registered": 0,
         "maximum": 45
     },
     {
         "subjectID": "240-306",
         "subjectName": "Wireless and Mobile Networks",
-        "weight": 2,
+        "credit": 2,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-307",
         "subjectName": "Computer Architecture and Organization",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 70
     },
     {
         "subjectID": "240-308",
         "subjectName": "Computer Engineering Project Preparation",
-        "weight": 2,
+        "credit": 2,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 19,
+        "registered": 0,
         "maximum": 50
     },
     {
         "subjectID": "240-309",
         "subjectName": "Microcontroller and Interfacing",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 52,
+        "registered": 0,
         "maximum": 60
     },
     {
         "subjectID": "240-310",
         "subjectName": "Algorithms: Design and Analysis",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 41,
+        "registered": 0,
         "maximum": 71
     },
     {
         "subjectID": "240-311",
         "subjectName": "Distributed Computing and Web Technologies",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
-        "registered": 51,
+        "registered": 0,
         "maximum": 70
     },
     {
         "subjectID": "240-312",
         "subjectName": "Computer Security",
-        "weight": 3,
+        "credit": 3,
         "departure": "Computer Engineering",
         "faculty": "Engineering",
         "registered": 51,
@@ -275,14 +275,72 @@ var isValidSubject = async (subject_id) => {
 
 // app.use(cors());
 
-//initialize web server & redis
+//initialize web 
 router.route('/')
     .get((req, res) => {
         res.send("Hello!, Welcome to RegisterApp API");
     }
-    );
+    )
 
 
+
+
+
+
+/*---------------------------------------------- Admin ----------------------------------------------*/
+//add subject
+router.route('/add-subject')
+    .post((req, res) => {
+        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+            (err, db) => {
+                if (err) throw err;
+                var dbo = db.db("course");
+                dbo.collection("subjects").insertOne({
+                    "subjectID": req.body.subjectID,
+                    "subjectName": req.body.subjectName,
+                    "credit": req.body.credit,
+                    "departure": req.body.departure,
+                    "faculty": req.body.faculty,
+                    "registered": req.body.registered,
+                    "maximum": req.body.maximum
+                }, (err, subject) => {
+                    dbo.collection("subjects").findOne({ "subjectID": req.body.subjectID }, (err, subject) => {
+                        if (err) throw err;
+                        res.json(subject);
+                        db.close();
+                    });
+                });
+            }
+        );
+    })
+
+//add all
+router.route('/add-all')
+    .get((req, res) => {
+        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+            (err, db) => {
+                if (err) throw err;
+                var dbo = db.db("course");
+                dbo.collection('subjects').insertMany(subjects);
+                res.send(subjects);
+            }
+        );
+    })
+//delete all
+router.route('/delete-all')
+    .get((req, res) => {
+        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+            (err, db) => {
+                if (err) throw err;
+                var dbo = db.db("course");
+                dbo.collection('subjects').drop();
+                res.send("All Subjects are deleted.");
+            }
+        );
+    })
+
+
+/*------------------------------------------ Client ---------------------------------------------------*/
 //get all subjects in database
 router.route('/subjects')
     .get((req, res) => {
@@ -297,31 +355,7 @@ router.route('/subjects')
                 })
             }
         );
-    });
-
-router.route('/add')
-    .get((req, res) => {
-        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
-            (err, db) => {
-                if (err) throw err;
-                var dbo = db.db("course");
-                dbo.collection('subjects').insert(subjects);
-                res.send(subjects);
-            }
-        );
-    });
-
-router.route('/delete')
-    .get((req, res) => {
-        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
-            (err, db) => {
-                if (err) throw err;
-                var dbo = db.db("course");
-                dbo.collection('subjects').drop();
-                res.send("All Subjects are deleted.");
-            }
-        );
-    });
+    })
 
 //to get subject by id
 router.route('/subjects/:id')
@@ -342,7 +376,33 @@ router.route('/subjects/:id')
         } else {
             res.status(404).send("Cannot find this course.");
         }
-    });
+    })
+
+/*----------------------------- Admin ------------------------------------*/
+// delete subject
+    .delete((req, res) => {
+
+        var subject_id = req.params.id;
+        if (isValidSubject(subject_id)) {
+            MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+                (err, db) => {
+                    if (err) throw err;
+                    var dbo = db.db("course");
+                    dbo.collection("subjects").findOne({ "subjectID": subject_id }, (err, subject) => {
+                        if (err) throw err;
+                        dbo.collection('subjects').deleteOne(subject);
+                        res.send("Delete this subject");
+                    });
+                }
+            );
+        } else {
+            res.status(404).send("Cannot find this course.");
+        }
+    })
+
+/*------------------------------------------------------------------------*/
+
+
 
 app.use("*", (req, res) => res.status(404).send('404 Not found'));
 app.listen(80, () => console.log('server is running...'))
